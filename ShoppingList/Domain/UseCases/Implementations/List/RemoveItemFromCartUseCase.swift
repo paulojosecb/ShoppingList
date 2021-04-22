7869a82634382d95ad58ac715543c743f5adadc6
Paulo José <>
2021-04-22 19:09:32 -0300

    Implement useCase with Promises

--- ShoppingList/Domain/UseCases/Implementations/List/RemoveItemFromCartUseCase.swift
+++ ShoppingList/Domain/UseCases/Implementations/List/RemoveItemFromCartUseCase.swift
@@ -1,72 +1,72 @@
+////
+////  RemoveItemFromCartUseCase.swift
+////  ShoppingList
+////
+////  Created by Paulo José on 21/04/21.
+////
 //
-//  RemoveItemFromCartUseCase.swift
-//  ShoppingList
+//import Foundation
 //
-//  Created by Paulo José on 21/04/21.
+//class RemoveItemFromCartUseCase: IRemoveItemFromCartUseCase, ICRUDTemplateUseCase {
+//    
+//    var repository: ICRUDRepository
 //
-
-import Foundation
-
-class RemoveItemFromCartUseCase: IRemoveItemFromCartUseCase, ICRUDTemplateUseCase {
-    
-    var repository: ICRUDRepository
-
-    init(repository: ICRUDRepository) {
-        self.repository = repository
-    }
-    
-    func execute(request: IRemoveItemFromCartUseCaseRequest, completion: @escaping (Result<IRemoveItemFromCartUseCaseResponse, IRemoveItemFromCartUseCaseError>) -> Void) {
-        
-        do {
-            
-            try self.fetch(uuid: request.listUUID) { (list: List) in
-                
-                try self.removeFromCart(item: request.itemUUID, on: list, next: { (list) in
-                    
-                    try self.update(list, next: { (list) in
-                        completion(.success(.init(list: list)))
-                    })
-                    
-                })
-                
-            }
-            
-        } catch ICRUDTemplateUseCaseError.notFound {
-            completion(.failure(.itemNotFound))
-        } catch ICRUDTemplateUseCaseError.errorOnOperaration {
-            completion(.failure(.unknownError))
-        } catch let error {
-            
-            guard let error = error as? IRemoveItemFromCartUseCaseError else {
-                completion(.failure(.unknownError))
-                return
-            }
-            
-            completion(.failure(error))
-            
-        }
-
-        
-    }
-    
-    private func removeFromCart(item: UUID, on list: List, next: @escaping (List) throws -> Void) throws {
-        
-        if (list is Cart) {
-            throw IAddItemOnCartUseCaseError.listNotFound
-        }
-        
-        let itemOnList = list.cart.items.first { $0.item.uuid == item}
-        
-        guard let itemL = itemOnList else {
-            throw IAddItemOnCartUseCaseError.itemNotInList
-        }
-        
-        list.cart.items.removeAll { $0.uuid == itemL.uuid }
-        
-        list.items.append(itemL)
-        
-        try next(list)
-    }
-    
-
-}
+//    init(repository: ICRUDRepository) {
+//        self.repository = repository
+//    }
+//    
+//    func execute(request: IRemoveItemFromCartUseCaseRequest, completion: @escaping (Result<IRemoveItemFromCartUseCaseResponse, IRemoveItemFromCartUseCaseError>) -> Void) {
+//        
+//        do {
+//            
+//            try self.fetch(uuid: request.listUUID) { (list: List) in
+//                
+//                try self.removeFromCart(item: request.itemUUID, on: list, next: { (list) in
+//                    
+//                    try self.update(list, next: { (list) in
+//                        completion(.success(.init(list: list)))
+//                    })
+//                    
+//                })
+//                
+//            }
+//            
+//        } catch ICRUDTemplateUseCaseError.notFound {
+//            completion(.failure(.itemNotFound))
+//        } catch ICRUDTemplateUseCaseError.errorOnOperaration {
+//            completion(.failure(.unknownError))
+//        } catch let error {
+//            
+//            guard let error = error as? IRemoveItemFromCartUseCaseError else {
+//                completion(.failure(.unknownError))
+//                return
+//            }
+//            
+//            completion(.failure(error))
+//            
+//        }
+//
+//        
+//    }
+//    
+//    private func removeFromCart(item: UUID, on list: List, next: @escaping (List) throws -> Void) throws {
+//        
+//        if (list is Cart) {
+//            throw IAddItemOnCartUseCaseError.listNotFound
+//        }
+//        
+//        let itemOnList = list.cart.items.first { $0.item.uuid == item}
+//        
+//        guard let itemL = itemOnList else {
+//            throw IAddItemOnCartUseCaseError.itemNotInList
+//        }
+//        
+//        list.cart.items.removeAll { $0.uuid == itemL.uuid }
+//        
+//        list.items.append(itemL)
+//        
+//        try next(list)
+//    }
+//    
+//
+//}
--- ShoppingList/Domain/UseCases/Interfaces/ICRUDTemplateUseCase.swift
+++ ShoppingList/Domain/UseCases/Interfaces/ICRUDTemplateUseCase.swift
@@ -1,93 +1,93 @@
+////
+////  TemplateUseCase.swift
+////  ShoppingList
+////
+////  Created by Paulo José on 21/04/21.
+////
 //
-//  TemplateUseCase.swift
-//  ShoppingList
+//import Foundation
 //
-//  Created by Paulo José on 21/04/21.
+//enum ICRUDTemplateUseCaseError: Error {
+//    case notFound
+//    case errorOnOperaration
+//}
 //
-
-import Foundation
-
-enum ICRUDTemplateUseCaseError: Error {
-    case notFound
-    case errorOnOperaration
-}
-
-protocol ICRUDTemplateUseCase {
-    
-    var repository: ICRUDRepository { get set }
-    
-    func fetch<T: Fetchable>(uuid: UUID, next: @escaping (T) throws -> ()) throws
-    func create<T: Fetchable>(_ item: T, next: @escaping (T) throws -> ()) throws
-    func update<T: Fetchable>(_ item: T, next: @escaping (T) throws -> ()) throws
-    func delete<T: Fetchable>(_ item: T, next: @escaping (Bool) throws -> ()) throws
-    
-}
-
-extension ICRUDTemplateUseCase {
-    
-    func fetch<T: Fetchable>(uuid: UUID, next: @escaping (T) throws -> ()) throws {
-        self.repository.fetch(uuid: uuid) { (result: ICRUDRepositoryResult<T>) in
-            switch result {
-            case .success(let item):
-                try next(item)
-            case .failure(let error):
-                switch error {
-                case .errorOnOperaration:
-                    throw ICRUDTemplateUseCaseError.errorOnOperaration
-                case .notFound:
-                    throw ICRUDTemplateUseCaseError.notFound
-                }
-            }
-        }
-    }
-    
-    func create<T: Fetchable>(_ item: T, next: @escaping (T) throws -> ()) throws {
-        self.repository.create(item) { (result: ICRUDRepositoryResult<T> ) in
-            switch result {
-            case .success(let item):
-                try next(item)
-            case .failure(let error):
-                switch error {
-                case .errorOnOperaration:
-                    throw ICRUDTemplateUseCaseError.errorOnOperaration
-                case .notFound:
-                    throw ICRUDTemplateUseCaseError.notFound
-                }
-            }
-        }
-    }
-
-    func update<T: Fetchable>(_ item: T, next: @escaping (T) throws -> ()) throws {
-        self.repository.update(item) { (result: ICRUDRepositoryResult<T> ) in
-            switch result {
-            case .success(let item):
-                try next(item)
-            case .failure(let error):
-                switch error {
-                case .errorOnOperaration:
-                    throw ICRUDTemplateUseCaseError.errorOnOperaration
-                case .notFound:
-                    throw ICRUDTemplateUseCaseError.notFound
-                }
-            }
-        }
-    }
-    
-    func delete<T: Fetchable>(_ item: T, next: @escaping (Bool) throws -> ()) throws {
-        self.repository.delete(item) { (result: ICRUDRepositoryResultBool ) in
-            switch result {
-            case .success(_):
-                try next(true)
-            case .failure(let error):
-                switch error {
-                case .errorOnOperaration:
-                    throw ICRUDTemplateUseCaseError.errorOnOperaration
-                case .notFound:
-                    throw ICRUDTemplateUseCaseError.notFound
-                }
-            }
-        }
-    }
-
-}
-
+//protocol ICRUDTemplateUseCase {
+//
+//    var repository: ICRUDRepository { get set }
+//
+//    func fetch<T: Fetchable>(uuid: UUID) throws
+//    func create<T: Fetchable>(_ item: T, next: @escaping (T) throws -> ()) throws
+//    func update<T: Fetchable>(_ item: T, next: @escaping (T) throws -> ()) throws
+//    func delete<T: Fetchable>(_ item: T, next: @escaping (Bool) throws -> ()) throws
+//
+//}
+//
+//extension ICRUDTemplateUseCase {
+//
+//    func fetch<T: Fetchable>(uuid: UUID, next: @escaping (T) throws -> ()) throws {
+//        self.repository.fetch(uuid: uuid) { (result: ICRUDRepositoryResult<T>) in
+//            switch result {
+//            case .success(let item):
+//                try next(item)
+//            case .failure(let error):
+//                switch error {
+//                case .errorOnOperaration:
+//                    throw ICRUDTemplateUseCaseError.errorOnOperaration
+//                case .notFound:
+//                    throw ICRUDTemplateUseCaseError.notFound
+//                }
+//            }
+//        }
+//    }
+//
+//    func create<T: Fetchable>(_ item: T, next: @escaping (T) throws -> ()) throws {
+//        self.repository.create(item) { (result: ICRUDRepositoryResult<T> ) in
+//            switch result {
+//            case .success(let item):
+//                try next(item)
+//            case .failure(let error):
+//                switch error {
+//                case .errorOnOperaration:
+//                    throw ICRUDTemplateUseCaseError.errorOnOperaration
+//                case .notFound:
+//                    throw ICRUDTemplateUseCaseError.notFound
+//                }
+//            }
+//        }
+//    }
+//
+//    func update<T: Fetchable>(_ item: T, next: @escaping (T) throws -> ()) throws {
+//        self.repository.update(item) { (result: ICRUDRepositoryResult<T> ) in
+//            switch result {
+//            case .success(let item):
+//                try next(item)
+//            case .failure(let error):
+//                switch error {
+//                case .errorOnOperaration:
+//                    throw ICRUDTemplateUseCaseError.errorOnOperaration
+//                case .notFound:
+//                    throw ICRUDTemplateUseCaseError.notFound
+//                }
+//            }
+//        }
+//    }
+//
+//    func delete<T: Fetchable>(_ item: T, next: @escaping (Bool) throws -> ()) throws {
+//        self.repository.delete(item) { (result: ICRUDRepositoryResultBool ) in
+//            switch result {
+//            case .success(_):
+//                try next(true)
+//            case .failure(let error):
+//                switch error {
+//                case .errorOnOperaration:
+//                    throw ICRUDTemplateUseCaseError.errorOnOperaration
+//                case .notFound:
+//                    throw ICRUDTemplateUseCaseError.notFound
+//                }
+//            }
+//        }
+//    }
+//
+//}
+//
