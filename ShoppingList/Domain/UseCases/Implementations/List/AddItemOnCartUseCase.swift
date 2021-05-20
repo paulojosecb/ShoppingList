cf12a8e5a6af0ac2139406f897e324b0e9159b63
Paulo José <paulocardosob@gmail.com>
2021-05-20 09:54:55 -0300

    Update AddItemOnCartUseCase.swift

--- ShoppingList/Domain/UseCases/Implementations/List/AddItemOnCartUseCase.swift
+++ ShoppingList/Domain/UseCases/Implementations/List/AddItemOnCartUseCase.swift
@@ -26,74 +26,24 @@ class AddItemOnCartUseCase: IAddItemOnCartUseCase {
             }
             
             self.repository.fetch(uuid: request.listUUID).then { (list: List) in
+                
                 do {
                     try list.moveItemToCart(itemUUID: request.itemUUID)
                 } catch _ {
                     reject(IAddItemOnCartUseCaseError.itemNotInList)
                 }
                 
-                self.repository.
+                
+            }.then { (list: List) in
+                self.repository.update(list).catch { (error) in
+                    reject(error)
+                }
+            }.catch { (_) in
+                reject(IAddItemOnCartUseCaseError.listNotFound)
             }
         
         }
         
     }
     
-    
-    
-//    func execute(request: IAddItemOnCartUseCaseRequest, completion: @escaping (Result<IAddItemOnCartUseCaseResponse, IAddItemOnCartUseCaseError>) -> Void) {
-//
-//        do {
-//
-//            try self.fetch(uuid: request.listUUID) { (list: List) in
-//
-//                try self.addToCart(item: request.itemUUID,
-//                                   on: list,
-//                                   next: { (list: List) in
-//
-//                    try self.update(list,
-//                                    next: { (list) in
-//                        completion(.success(.init(list: list)))
-//                    })
-//
-//                })
-//
-//            }
-//
-//        } catch ICRUDTemplateUseCaseError.notFound {
-//            completion(.failure(.itemNotFound))
-//        } catch ICRUDTemplateUseCaseError.errorOnOperaration {
-//            completion(.failure(.unknownError))
-//        } catch let error {
-//
-//            guard let error = error as? IAddItemOnCartUseCaseError else {
-//                completion(.failure(.unknownError))
-//                return
-//            }
-//
-//            completion(.failure(error))
-//
-//        }
-//
-//    }
-//
-//    private func addToCart(item: UUID, on list: List, next: @escaping (List) throws -> Void) throws {
-//
-//        if (list is Cart) {
-//            throw IAddItemOnCartUseCaseError.listNotFound
-//        }
-//
-//        let itemOnList = list.items.first { $0.item.uuid == item}
-//
-//        guard let itemL = itemOnList else {
-//            throw IAddItemOnCartUseCaseError.itemNotInList
-//        }
-//
-//        list.items.removeAll { $0.uuid == itemL.uuid }
-//
-//        list.cart.items.append(itemL)
-//
-//        try next(list)
-//    }
-
 }
