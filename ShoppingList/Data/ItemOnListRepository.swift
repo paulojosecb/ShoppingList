bc569bfac02c77976d4b581a49f5ffcecc8cbcb1
Paulo José <paulocardosob@gmail.com>
2021-05-21 16:44:24 -0300

    Implement fetch and create method on ItemOnListRepository

--- ShoppingList/Data/ItemOnListRepository.swift
+++ ShoppingList/Data/ItemOnListRepository.swift
@@ -22,31 +22,47 @@ class ItemOnListRepository: ICRUDRepository {
     
     func fetch<T>(uuid: String) -> Promise<T> where T : Fetchable {
         return Promise { fulfill, reject in
+            let cdItemOnList: CDItemOnList? = self.coreDataStack.fetch(by: uuid)
             
+            guard cdItemOnList != nil else {
+                reject(ICRUDRepositoryError.notFound)
+                return
+            }
+            
+            fulfill(ItemOnList.make(from: cdItemOnList!) as! T)
         }
     }
     
     func fetch<T: Fetchable>(uuids: String) -> Promise<[T]> {
         return Promise { fulfill, reject in
-            
+            fatalError()
         }
     }
     
     func create<T>(_ item: T) -> Promise<T> where T : Fetchable {
         return Promise { fulfill, reject in
+            let cdItemOnList = CDItemOnList(context: self.coreDataStack.mainContext)
             
+            let _ = ItemOnList.make(from: item as! ItemOnList, cdItemOnList: cdItemOnList)
+            
+            do {
+                try self.coreDataStack.mainContext.save()
+                fulfill(item)
+            } catch _ {
+                reject(ICRUDRepositoryError.errorOnOperaration)
+            }
         }
     }
     
     func update<T>(_ item: T) -> Promise<T> where T : Fetchable {
         return Promise { fulfill, reject in
-            
+            fatalError()
         }
     }
     
     func delete<T>(_ item: T) -> Promise<Bool> where T : Fetchable {
         return Promise { fulfill, reject in
-            
+            fatalError()
         }
     }
     
